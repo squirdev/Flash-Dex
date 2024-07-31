6bc75a292d516b64a1c023736f3c961322e8b6f4
squirdev <kevinlee2821@gmail.com>
2024-07-31 04:54:32 -0700

    remove some tesxt

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,27 +2,36 @@ import LogoImg from "../../src/assets/images/logo.png"
 import WalletConnectBtn from "./WalletConnectBtn"
 import HistoryImg from "../../src/assets/images/history.png"
 import FlashImg from "../../src/assets/images/flash.svg"
+import { useMediaQuery } from 'react-responsive'
 
 export default function Header() {
+    const isDesktopOrTablet = useMediaQuery({ query: '(max-width: 1224px)' })
+    const isTabletOrMobile = useMediaQuery({ query: '(max-width: 500px)' })
 
     return (
         <div className="absolute top-0 flex flex-row items-center justify-between p-[20px] h-[60px] w-full bg-[#06070A] ">
             <img src={LogoImg} alt="logo" />
             <div className="flex flex-row items-center gap-2">
-                <div className="flex flex-row items-center gap-1">
-                    <img src={FlashImg} className="rounded-full w-[30px] h-[30px] bg-white" />
-                    <p className="text-[16px] text-white  font-Roboto font-[400]">
-                        $0.000317
-                    </p>
-                </div>
-                <button className="flex flex-col items-center justify-center p-[10px] rounded-full border-[1px] border-white w-[132px] h-[46px]">
-                    <div className="flex flex-row items-center gap-2">
-                        <img src={HistoryImg} alt="history" />
-                        <p className="text-white text-[14px] font-Roboto font-[500]">
-                            Tx History
+                {
+                    !isTabletOrMobile &&
+                    <div className="flex flex-row items-center gap-1">
+                        <img src={FlashImg} className="rounded-full w-[30px] h-[30px] bg-white" />
+                        <p className="text-[16px] text-white  font-Roboto font-[400]">
+                            $0.000317
                         </p>
                     </div>
-                </button>
+                }
+                {
+                    !isDesktopOrTablet &&
+                    <button className="flex flex-col items-center justify-center p-[10px] rounded-full border-[1px] border-white w-[132px] h-[46px]">
+                        <div className="flex flex-row items-center gap-2">
+                            <img src={HistoryImg} alt="history" />
+                            <p className="text-white text-[14px] font-Roboto font-[500]">
+                                Tx History
+                            </p>
+                        </div>
+                    </button>
+                }
                 <WalletConnectBtn />
             </div>
         </div>
--- src/pages/MainScene.tsx
+++ src/pages/MainScene.tsx
@@ -8,23 +8,35 @@ import { LiFiWidget } from '@lifi/widget';
 export default function MainScene() {
     const [showModal, setShowModal] = useState(false);
     const { openConnectModal } = useConnectModal();
+
+
+
     return (
         <div className="flex items-center justify-center h-screen w-full bg-[#06070A] ">
             <Header />
             {/* <MainPanel showModal={showModal} setShowModal={setShowModal} /> */}
 
-            <LiFiWidget
-                config={{
-                    appearance: 'dark',
-                    theme: {
-                        container: {
-                            boxShadow: '0px 8px 32px rgba(0, 0, 0, 0.08)',
-                            borderRadius: '16px',
+            <div className="w-full px-[20px]">
+                <LiFiWidget
+                    config={{
+                        walletConfig: {
+                            onConnect() {
+                                openConnectModal?.();
+                            },
                         },
-                    },
-                }}
-                integrator="cra-example"
-            />
+                        appearance: 'dark',
+                        theme: {
+                            container: {
+                                boxShadow: '0px 8px 32px rgba(0, 0, 0, 0.08)',
+                                borderRadius: '16px',
+                                borderWidth: '1px',
+                                borderColor: '#444'
+                            },
+                        },
+                    }}
+                    integrator="cra-example"
+                />
+            </div>
 
         </div>
     )
